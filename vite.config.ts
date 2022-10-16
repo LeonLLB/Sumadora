4112b4cd063690b589786d7f736da5ff4ce6d2c4
Leonard Velásquez <leonard.avp@gmail.com>
2022-10-16 19:22:16 -0400

    Cambios de PWA 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions:{
+      //   enabled:true,
+      //   type: 'module',
+      //   navigateFallback: 'index.html',
+      // },
+      base: '/',
+      srcDir:'src',
+      filename:'serviceWorker.ts',
+      includeAssets: ['favicon.svg'],
+      strategies:'injectManifest',
+      manifest: {
+        name: 'Sumadora Virtual',
+        short_name: 'VSumadora',
+        theme_color: '#ffffff',
+        // icons: [
+        //   {
+        //     src: 'pwa-192x192.png', // <== don't add slash, for testing
+        //     sizes: '192x192',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png', // <== don't add slash, for testing
+        //     sizes: '512x512',
+        //     type: 'image/png',
+        //     purpose: 'any maskable',
+        //   },
+        // ],
+      },   
+    })
+  ]
 })
